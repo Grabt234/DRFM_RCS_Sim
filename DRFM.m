da754968e4df8cca62ab7f579a732bd935de9937
unknown <gnorrie12@gmail.com>
2021-08-01 14:03:04 +0200

    1.1-1.2 partially completed

--- DRFM.m
+++ DRFM.m
@@ -8,7 +8,7 @@
 %all sinusoids have magnitude 1
 
 %sampling rate
-fs = 50000;
+fs = 100000;
 %sampling preiod
 T = 1/fs;
 %run time
@@ -62,9 +62,9 @@ LF_W = fftshift(fft(LF_p));
 IF_W = fftshift(fft(IF_p));
 HF_W = fftshift(fft(HF_p));
 
+%frequency axis
 f_res = 1/(length(LF_W)*T);
 f = 0:f_res:(length(LF_W)-1)*f_res;
-
 %shifting to center arround zero
 f = f - f((length(f))/2);
 %rescaling to KHz
@@ -89,18 +89,68 @@ xlabel("frequency(Khz)")
 ylabel("Amplitude(V)")
 title("FFT OF 10KHz SINUSOID")
 
-%% ADC QUANTIZATION
-N_bits = 1;
-N_levels = 2^N_bits;
+%% Question 1.1 - 1.2
+
+pad_factor = 5;
+
+%----------------------------------------------
+%One signal using a *1* bit A/D converter: 5KHz
+%----------------------------------------------
+bits = 1;
+amplitude = 1;
+IF_1b = IF;
+
+%time domain
+IF_1b= adc_sample(bits,amplitude,IF_1b);
+%frequency domain
+IF_1b_p = [IF_1b zeros(1,length(IF_1b)*pad_factor)];
+IF_1b_w = fftshift(fft(IF_1b_p));
 
 
-%% SINGLE SIGNAL - 1 BIT
+%----------------------------------------------
+%One signal using a *5* bit A/D converter: 5KHz
+%----------------------------------------------
+bits = 5;
+amplitude = 1;
+IF_5b = IF;
 
+%time domain
+IF_5b = adc_sample(bits,amplitude,IF_5b);
+%frequency domain
+IF_5b_p = [IF_5b zeros(1,length(IF_5b)*pad_factor)];
+IF_5b_w = fftshift(fft(IF_5b_p));
 
-adc_1 = adc_sample(5,1,LF);
+%----------------------------------------------
+%plotting
+%----------------------------------------------
+
+%frequency axos
+f_res = 1/(length(IF_1b)*T);
+f = 0:f_res:(length(IF_1b_w)-1)*f_res;
+%shifting to center arround zero
+f = f - f((length(f))/2);
+%rescaling to KHz
+f_khz = f/1000;
 
 figure
-plot(1:1:length(LF),adc_1)
+subplot(2,2,1)
+plot(t,IF_1b)
+xlabel("Time(ms)")
+ylabel("Amplitude(V)")
+title("TIME DOMAIN 5KHz SINUSOID - 1 BIT ADC")
+
+subplot(2,2,2)
+plot(t,IF_5b)
+xlabel("Time(ms)")
+ylabel("Amplitude(V)")
+title("TIME DOMAIN 5KHz SINUSOID - 5 BIT ADC")
+
+subplot(2,2,3)
+plot(f,abs(IF_1b_w))
+
+subplot(2,2,4)
+plot(f,abs(IF_5b_w))
+
 
 
 
