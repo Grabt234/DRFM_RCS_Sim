1e5ec89d405ba59f1651081feaed7c4523a54fd7
unknown <gnorrie12@gmail.com>
2021-08-01 15:20:33 +0200

    added RCS question

---
+++ RCS.m
@@ -0,0 +1,16 @@
+%%
+%--------------------------------------------------------------------------
+% Matlab code investigating RCS Shape Evaluation
+
+% Plot the Radar Cross Section (RCS) radiation patterns for both a sphere 
+% and a flat plate at f= 12.5 GHz. The patterns  should  cover  aspect  
+% angles  relative  to  the  peak  (or  bore  sight)  with θ∈(-90° :90°)  
+% in  either  the vertical  or  horizontal  plane.  The  sphere  has  a  
+% radius  of  0.75  m.  The  flat  plate  is  square  with  a  height  
+% (or width) of 0.4 m and thickness of 5 mm. Compare your results for each
+% shape and recommend a shape to be used for RCS reduction.Also recommend 
+% a shape for calibrating a radar.  Be sure to give reasons why you would 
+% choose the one shape over the other
+%--------------------------------------------------------------------------
+
+%%
